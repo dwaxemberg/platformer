dad97a6b3359f94851fbabe93ab6680d0b049278
chas11man <chas11man@gmail.com>
2013-02-19 23:37:40 -0500

    Forward and backward movement implemented with W and S keys. Left and right turn working with A and S keys but trying to make the turning movement smooth.

--- Platform Game/Assets/MainCharacter.cs
+++ Platform Game/Assets/MainCharacter.cs
@@ -11,31 +11,32 @@ public class MainCharacter : MonoBehaviour {
 	 * need to add jumping. Make it 2D movement, i.e. left and right (pan) rotate 180 degrees
 	 */
 
-	
+	Quaternion rightRotation = Quaternion.Euler(0,180,0);
+	Quaternion leftRotation = Quaternion.Euler(0,-180,0);
 	// Update is called once per frame
 	void Update () {
-		var position = this.transform.position;
-		bool rotated = false;
 		if (Input.GetKey(KeyCode.W)) {
 			// move forward.
 			this.transform.Translate(new Vector3(0F, 0F, 1F) * VELOCITY);
 		}
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKeyDown (KeyCode.D)) {
 			// turn right
-			this.transform.Rotate(new Vector3(0f, 1f, 0F), 5);
+			var startRotation = this.transform.rotation;
+			var endRotation = this.transform.rotation * Quaternion.Euler(0,180,0);
+			var rate = 1;
+			float t = 0;
+			while(t < 1) {
+				t += (Time.deltaTime * rate);
+				this.transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+			}
 		}
-		if (Input.GetKey(KeyCode.A)) {
+		if (Input.GetKeyDown(KeyCode.A)) {
 			// turn left
-			this.transform.Rotate(new Vector3(0f, 1f, 0f), -5);
+			this.transform.Rotate(new Vector3(0f, 1f, 0f), -180);
 		}
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (Input.GetKey(KeyCode.S)) {
 			// turn around
-			this.transform.Rotate(new Vector3(0f, 1f, 0f), 180);
-			rotated = true;
+			this.transform.Translate(new Vector3(0f, 0f, 1f) * -VELOCITY);
 		}
-		if (Input.GetKeyUp(KeyCode.S)) {
-			rotated = false;
-		}
-		//random shit
 	}
 }
